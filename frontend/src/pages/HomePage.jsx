6b936ab01243fc30bfcd46f63b2e02082d00ee7e
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-11-14 16:15:10 -0300

    se añadieron los distintos eventos

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,51 +1,96 @@
+import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
+
 export const HomePage = () => {
   // TODO: Integrar lógica para obtener superhéroes desde la API
   // TODO: Implementar useState para almacenar la lista de superhéroes
   // TODO: Implementar función para recargar superhéroes
+  const [profile, setProfile] = useState({});
+  const [superHeroes, setSuperHeroes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Datos de ejemplo para las cards
-  const superheroes = [
-    {
-      id: 1,
-      superhero: "Superman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/644-superman.jpg",
-    },
-    {
-      id: 2,
-      superhero: "Batman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg",
-    },
-    {
-      id: 3,
-      superhero: "Wonder Woman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/720-wonder-woman.jpg",
-    },
-    {
-      id: 4,
-      superhero: "Spider-Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/620-spider-man.jpg",
-    },
-    {
-      id: 5,
-      superhero: "Iron Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/346-iron-man.jpg",
-    },
-    {
-      id: 6,
-      superhero: "Captain America",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/149-captain-america.jpg",
-    },
-  ];
+  // const superheroes = [
+  //   {
+  //     id: 1,
+  //     superhero: "Superman",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/644-superman.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     superhero: "Batman",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     superhero: "Wonder Woman",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/720-wonder-woman.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     superhero: "Spider-Man",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/620-spider-man.jpg",
+  //   },
+  //   {
+  //     id: 5,
+  //     superhero: "Iron Man",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/346-iron-man.jpg",
+  //   },
+  //   {
+  //     id: 6,
+  //     superhero: "Captain America",
+  //     image:
+  //       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/149-captain-america.jpg",
+  //   },
+  // ];
+  const getDataProfile = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/profile", {
+        method: "GET",
+        credentials: "include",
+      });
 
+      const data = await response.json();
+      console.log("Home", data);
+      setProfile(data.user);
+    } catch (error) {
+      console.error("Error al trar el perfil:", error);
+    }
+  };
+  const getDataSuperHeroes = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:3000/api/superheroes", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const data = await response.json();
+      setSuperHeroes(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error al trar el perfil:", error);
+    }
+  };
+  useEffect(() => {
+    getDataProfile();
+    getDataSuperHeroes();
+  }, []);
+
+  const handleReload = () => {
+    setSuperHeroes([]);
+    console.log(superHeroes);
+    getDataSuperHeroes();
+  };
   return (
     <div className="container mx-auto px-4 pb-8">
       <h1 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
+        ¡Bienvenido/a, {profile.name}! <br />
         Galería de Superhéroes
       </h1>
 
@@ -53,6 +98,7 @@ export const HomePage = () => {
         <button
           onClick={() => {
             // TODO: Implementar función para recargar superhéroes
+            handleReload();
           }}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded transition-colors"
         >
@@ -60,26 +106,30 @@ export const HomePage = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {superheroes.map((hero) => (
-          <div
-            key={hero.id}
-            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer"
-          >
-            <img
-              src={hero.image}
-              alt={hero.superhero}
-              className="h-64 object-cover w-full"
-            />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {superHeroes.map((hero) => (
+            <div
+              key={hero.id}
+              className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer"
+            >
+              <img
+                src={hero.image}
+                alt={hero.superhero}
+                className="h-64 object-cover w-full"
+              />
 
-            <div className="p-4">
-              <h3 className="text-xl font-semibold text-gray-800">
-                {hero.superhero}
-              </h3>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold text-gray-800">
+                  {hero.superhero}
+                </h3>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
